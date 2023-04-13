a39e2e290725f8dbc06232df72a38fa4901c42d8
Bozhecarayhrani <81078142+Bozhecarayhrani@users.noreply.github.com>
2023-04-13 22:46:43 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -9,4 +9,4 @@ def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return stats.ks_2samp(x, y)[1] > 0.05 # Ваш ответ, True или False
+    return stats.anderson_ksamp([x, y])[1] > 0.05 # Ваш ответ, True или False
